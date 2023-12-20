0ec53db9c1a47fd17dc18551dfcc1bcbd7a30d5a
gokul18 <gokulakrishnan.doodleblue@gmail.com>
2023-12-20 10:28:58 +0530

    sdfsd

--- src/screens/OnboardingScreens/LoginScreen/index.js
+++ src/screens/OnboardingScreens/LoginScreen/index.js
@@ -1,13 +1,15 @@
-import React, {useState} from 'react';
-import {ImageBackground, Image, Text, TextInput, View} from 'react-native';
-import {TouchableOpacity} from 'react-native-gesture-handler';
+import React, { useState } from 'react';
+import { ImageBackground, Image, Text, TextInput, View } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 import firestore from '@react-native-firebase/firestore';
 import uuid from 'react-native-uuid';
-import {styles} from './styles';
+import { styles } from './styles';
 const LoginScreen = () => {
-  const [name, setName] = useState();
-  const [password, setPassword] = useState();
-  const [phoneNumber, setPhoneNumber] = useState();
+  const [name, setName] = useState("");
+  const [emailId, setEmailId] = useState("");
+  const [password, setPassword] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [confirmPassword, setconfirmPassword] = useState("")
 
   const register = () => {
     const UserID = uuid.v4();
@@ -16,42 +18,57 @@ const LoginScreen = () => {
       .doc(UserID)
       .set({
         name: name,
-        password: password,
+        email: emailId,
         phoneNumber: phoneNumber,
+        password: password,
+        confirmPassword: confirmPassword,
+
         userID: UserID,
       })
-      .then(resp => console.log(resp))
+      .then(resp => { console.log(resp) })
       .catch(errr => {
-        console.log(errr);
+        // console.log(errr);
+        console.log("errr");
+
       });
   };
   return (
     <View style={styles.body}>
       <Text style={styles.header}>
-        Pik<Text style={[styles.header, {color: '#20d5d8'}]}>up</Text>
+        Pik<Text style={[styles.header, { color: '#20d5d8' }]}>up</Text>
       </Text>
+      <TextInput
+        placeholder="email"
+        placeholderTextColor={'grey'}
+        style={styles.inputfiled}
+        onChangeText={value => setEmailId(value)}></TextInput>
       <TextInput
         placeholder="Name"
         placeholderTextColor={'grey'}
         style={styles.inputfiled}
-        onChange={value > setName(value)}></TextInput>
+        onChangeTextText={value => setName(value)}></TextInput>
       <TextInput
         placeholder="Phone Number"
         placeholderTextColor={'grey'}
         style={styles.inputfiled}
-        onChange={value > setPhoneNumber(value)}></TextInput>
+        onChangeText={value => setPhoneNumber(value)}></TextInput>
       <TextInput
         placeholder="Password"
         placeholderTextColor={'grey'}
         style={styles.inputfiled}
-        onChange={value > setPassword(value)}></TextInput>
+        onChangeText={value => setPassword(value)}></TextInput>
+      <TextInput
+        placeholder="Password"
+        placeholderTextColor={'grey'}
+        style={styles.inputfiled}
+        onChangeText={value => setconfirmPassword(value)}></TextInput>
 
       <View style={styles.button}>
         <TouchableOpacity style={styles.loginbtn} onPress={register}>
           <Text style={styles.btnlabel}>sign up</Text>
         </TouchableOpacity>
       </View>
-      <Text style={[styles.textlabel, {marginTop: 0}]}>Forget Password?</Text>
+      <Text style={[styles.textlabel, { marginTop: 0 }]}>Forget Password?</Text>
       <View
         style={{
           borderBottomWidth: 1,
@@ -73,10 +90,10 @@ const LoginScreen = () => {
           alignItems: 'flex-start',
         }}>
         <TouchableOpacity style={styles.signbtn}>
-          <Text style={{color: '#20d5d8'}}>GOOGLE</Text>
+          <Text style={{ color: '#20d5d8' }}>GOOGLE</Text>
         </TouchableOpacity>
         <TouchableOpacity style={styles.signbtn}>
-          <Text style={{color: '#20d5d8'}}>EMAIL</Text>
+          <Text style={{ color: '#20d5d8' }}>EMAIL</Text>
         </TouchableOpacity>
       </View>
     </View>
