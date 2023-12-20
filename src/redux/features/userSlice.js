44d19ddcf85d2233248807626716d7ef0d31d1b8
Sowmiya13G <sowmiyag13.doodleblue@gmail.com>
2023-12-20 15:38:59 +0530

    redux baseSetup

---
+++ src/redux/features/userSlice.js
@@ -0,0 +1,10 @@
+import {createSlice} from '@reduxjs/toolkit';
+
+const userSlice = createSlice({
+  name: 'users',
+  initialState: [],
+  reducers: {},
+});
+
+export const {} = itemSlice.actions;
+export default userSlice.reducer;
---
+++ src/redux/store.js
@@ -0,0 +1,28 @@
+import {configureStore, getDefaultMiddleware} from '@reduxjs/toolkit';
+import usersReducer from './features/userSlice';
+import {persistStore, persistReducer} from 'redux-persist';
+import {combineReducers} from 'redux';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+  whitelist: ['users'],
+};
+
+const rootReducer = combineReducers({
+  users: usersReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware({
+    serializableCheck: false,
+    serializableCheck: {
+      ignoredActions: ['persist/PERSIST'],
+    },
+  }),
+});
+
+export const persistor = persistStore(store);
